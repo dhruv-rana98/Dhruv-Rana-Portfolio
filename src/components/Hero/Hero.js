42c1c783bd04b4aabae95b8410813f06495bff14
Dhruv Rana <dhruvrana0000@gmail.com>
2022-07-26 12:55:32 +0530

    Creation of the Hero Component

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,13 +1,34 @@
-import React from 'react';
+import React from "react";
 
-import { Section, SectionText, SectionTitle } from '../../styles/GlobalComponents';
-import Button from '../../styles/GlobalComponents/Button';
-import { LeftSection } from './HeroStyles';
+import {
+  Section,
+  SectionText,
+  SectionTitle,
+} from "../../styles/GlobalComponents";
+import Button from "../../styles/GlobalComponents/Button";
+import { LeftSection } from "./HeroStyles";
 
-const Hero = (props) => (
-  <div>
-    Hero
-  </div>
+const Hero = () => (
+  <Section row nopadding>
+    <LeftSection>
+      <SectionTitle main center>
+        Welcome to <br />
+        The Personal Portfolio of Dhruv Rana
+      </SectionTitle>
+      <SectionText>
+        I am a frontend developer currently working at TCS aspiring for career
+        growth by building excellent web apps and providing solutions for
+        critical business needs.
+      </SectionText>
+      <Button
+        onClick={() =>
+          window.open("https://www.linkedin.com/in/dhruv-rana98/", "_blank")
+        }
+      >
+        Learn More
+      </Button>
+    </LeftSection>
+  </Section>
 );
 
 export default Hero;
